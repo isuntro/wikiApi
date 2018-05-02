775537588522291b0d547653931c8ad7afba3e29
Tiberiu Simion Voicu <tiberiusimionvoicu@gmail.com>
2018-05-02 16:12:00 +0100

    added tests config and env

--- api.py
+++ api.py
@@ -1,9 +1,25 @@
 #!envAPI/bin/python
 import sqlite3
 from flask import Flask, jsonify, make_response, g, request
+from config import app_config
 
 app = Flask(__name__)
-app.config.from_object(__name__)
+app.config.from_object(app_config["development"])
+app.config.from_pyfile('config.py')
+
+def connectDB():
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
+def initDB():
+    db = getDB()
+    with app.open_resource('schema.sql', mode='r') as f :
+        db.cursor().executescript(f.read())
+    db.commit()
+@app.cli.command('initDB')
+def initdbCommand():
+    initDB()
+    print('DB Initialised')
 
 @app.route('/documents')
 def getDocuments():
