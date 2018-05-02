6fd960a4057ab472b50a4c4fe52b30908f0b23fc
Tiberiu Simion Voicu <tiberiusimionvoicu@gmail.com>
2018-05-02 16:57:24 +0100

    implemented error handlers and end-points

--- api.py
+++ api.py
@@ -1,7 +1,8 @@
-#!envAPI/bin/python
+#!venv/bin/python
 import sqlite3
 from flask import Flask, jsonify, make_response, g, request
 from config import app_config
+from queries import *
 
 app = Flask(__name__)
 app.config.from_object(app_config["development"])
@@ -16,25 +17,62 @@ def initDB():
     with app.open_resource('schema.sql', mode='r') as f :
         db.cursor().executescript(f.read())
     db.commit()
+def getDB():
+    db = getattr(g, 'sqlite_db', None)
+    if db is None:
+        db = connectDB()
+    return db
+
 @app.cli.command('initDB')
 def initdbCommand():
     initDB()
     print('DB Initialised')
 
+@app.errorhandler(404)
+def notFound(error):
+    return make_response(jsonify({'error': "NOT FOUND"}),404)
+
 @app.route('/documents')
 def getDocuments():
-    return ''
+    ''' Return all documents '''
+    results = getDB().execute(getAllDocuments)
+    print(results.fetchall())
+    return jsonify(results.fetchall())
 @app.route('/documents/<string:title>')
 def getDocumentRevs(title):
-    return ''
+    ''' Return all versions of document with title '''
+    results = getDB().execute(getDocumentRevisions, {"title" : title})
+    return jsonify(results.fetchall())
+
 @app.route('/documents/<string:title>/<int:timestamp>')
 def getDocumentTime(title, timestamp):
-    return ''
+    ''' Return Document with title at given timestamp '''
+    if not title and not timestamp:
+        abort(404)
+    if timestamp > datetime.time() or len(title) < 1:
+        abort(404)
+    results = getDB().execute(getDocumentRevision, title, timestamp)
+    if not results:
+        abort(404)
+    return jsonify(results.fetchall())
 
 @app.route('/documents/<string:title>/latest')
 def getDocumentLatest():
+    ''' Return latest revision of document with title '''
     return 'Latest Document Revision'
 
 @app.route('/documents/<string:title>', methods=['POST'])
 def addDocument(title):
-    return ''
+    ''' Add a new document '''
+    if not request.json:
+        abort(400)
+    if len(title) > 50 or len(title) < 1:
+        abort(400)
+    document = {
+        'title': title,
+        'content': request.json['content'],
+        'tstamp': time.time()
+    }
+    cur = getDB().execute(insertDocument, document)
+    getDB().commit()
+    return jsonify(document),201
